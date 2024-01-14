0f32ae2b34b8ace78a1978f345ba4a1a94e59e77
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-01-14 12:31:56 +0800

    task(project): Create scaffold and Image

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,20 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
-      home: Center(
-          child: Text('Hello World')
-      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/272px-Cat_August_2010-4.jpg'),
+          ),
+        )
+      )
 
     )
   );
